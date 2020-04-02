1cd71abb0aee78d015add58576ef42832bfae55c
ignacio <ignacio@peletier.com>
2020-04-02 11:42:31 +0200

    xtransmit logs added to file

--- entrypoint_rcv.sh
+++ entrypoint_rcv.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir /logs/$ID
 timeout 30 tshark -i eth0 -f "port 1234" -s 1500 -w /logs/$ID/rcv-vSRT.pcapng &
-exec ./bin/srt-xtransmit --loglevel fatal receive "srt://1234?transtype=live&latency=$LATENCY" --msgsize 1316 --statsfile /dev/stdout --statsfreq $REPORT_FREQUENCY
+exec ./bin/srt-xtransmit --loglevel fatal receive "srt://1234?transtype=live&latency=$LATENCY" --msgsize 1316 --statsfile /logs/$ID/rcv-logs.csv --statsfreq $REPORT_FREQUENCY
--- entrypoint_snd.sh
+++ entrypoint_snd.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir /logs/$ID
 timeout 30 tshark -i eth0 -f "port $RECEIVER_PORT" -s 1500 -w /logs/$ID/snd-vSRT.pcapng &
-exec ./bin/srt-xtransmit --loglevel fatal generate "srt://$RECEIVER_ADDRESS:$RECEIVER_PORT?transtype=live&latency=$LATENCY" --msgsize 1316 --sendrate $SENDRATE --duration $DURATION  --statsfile /dev/stdout --statsfreq $REPORT_FREQUENCY
+exec ./bin/srt-xtransmit --loglevel fatal generate "srt://$RECEIVER_ADDRESS:$RECEIVER_PORT?transtype=live&latency=$LATENCY" --msgsize 1316 --sendrate $SENDRATE --duration $DURATION  --statsfile /logs/$ID/snd-logs.csv --statsfreq $REPORT_FREQUENCY
