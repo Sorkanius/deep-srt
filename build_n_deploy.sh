a5f4309f4b05029e7e7d742c21388a452636f346
ignacio <ignacio@peletier.com>
2020-04-02 12:07:28 +0200

    Reorganization of the repo

--- build_n_deploy.sh
+++ build_n_deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker build -f dockerfile-base -t base-srt:v2 .
-docker build -f dockerfile-receiver -t olympia.azurecr.io/receiver-srt:v2 . &&  docker push olympia.azurecr.io/receiver-srt:v2
-docker build -f dockerfile-transmitter -t olympia.azurecr.io/transmitter-srt:v2 . &&  docker push olympia.azurecr.io/transmitter-srt:v2
+docker build -f receiver/dockerfile-receiver -t olympia.azurecr.io/receiver-srt:v2 . &&  docker push olympia.azurecr.io/receiver-srt:v2
+docker build -f transmitter/dockerfile-transmitter -t olympia.azurecr.io/transmitter-srt:v2 . &&  docker push olympia.azurecr.io/transmitter-srt:v2
--- transmitter/build_transmitter.sh
+++ transmitter/build_transmitter.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -f dockerfile-base -t base-srt:v2 .
+docker build -f ../dockerfile-base -t base-srt:v2 .
 docker build -f dockerfile-transmitter -t transmitter-srt:v2 .
